d407b6bbefbdaf769aab0e45b139331840da4b68
Sara Alvarado Díaz <sr.lilitha@MBP-de-Sara.home>
2023-03-03 10:38:14 +0100

    Add push_swap.c

---
+++ push_swap.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    char    **args;
+    
+    if (argc < 2)
+        return (-1);
+    if (argc == 2)
+    {
+        args = ft_split(argv[1], ' ');
+        if (ft_check_errors(args, 0))
+            return (-1);
+        ft_free(args);
+    }
+    if (argc > 2)
+    {
+        args = argv;
+        if (ft_check_errors(args, 1))
+            return (-1);
+    }
+    return (0);
+}
