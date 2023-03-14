2921856efb5900262cfd12b8c268e996ac889399
Sara Alvarado Diaz <salvarad@c3r2s3.42malaga.com>
2023-03-14 12:16:54 +0100

    Update

--- push_swap.c
+++ push_swap.c
@@ -1,26 +1,22 @@
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: salvarad <salvarad@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 12:02:27 by salvarad          #+#    #+#             */
+/*   Updated: 2023/03/14 12:12:32 by salvarad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 int main(int argc, char **argv)
 {
-    char    **args;
-    
-    if (argc < 2)
-        return (-1);
-    if (argc == 2)
-    {
-        args = ft_split(argv[1], ' ');
-        if (ft_check_errors(args, 0))
-        {
-            ft_free(args);
-            return (-1);
-        }
-        ft_free(args);
-    }
-    if (argc > 2)
-    {
-        args = argv;
-        if (ft_check_errors(args, 1))
-            return (-1);
-    }
+	t_list	**stack_a;
+	t_list	**stack_b;
+	
+	stack_a = NULL;
+	stack_b = NULL;
+	ft_check_args(argc, argv);
     return (0);
 }
