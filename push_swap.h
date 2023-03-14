4a7150c679273e5692ffb6515bbad98ff93f9aec
Sara Alvarado Diaz <salvarad@c3r2s3.42malaga.com>
2023-03-14 12:18:24 +0100

    Add push_swap.h

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: salvarad <salvarad@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 10:52:43 by salvarad          #+#    #+#             */
+/*   Updated: 2023/03/14 11:01:00 by salvarad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "../libft/libft.h"
+
+typedef struct s_list
+{
+	int				value;
+	int				index;
+	struct s_list	*next;
+}	t_list;
