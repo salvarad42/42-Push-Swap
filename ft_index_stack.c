a80141194be37b2436bb67b87b242ed460f94007
Sara Alvarado Díaz <sr.lilitha@MBP-de-Sara.home>
2023-03-17 12:51:59 +0100

    Add ft_index_stack.c

---
+++ ft_index_stack.c
@@ -0,0 +1,33 @@
+#include "push_swap.h"
+
+static t_list   *ft_next_min(t_list **stack)
+{
+    t_list  *aux;
+    t_list  *min;
+
+    if (!*stack)
+        return (NULL);
+    aux = *stack;
+    min = aux;
+    while (aux)
+    {
+        if ((aux -> index == -1) && (aux -> value < min -> value))
+            min = aux;
+        aux = aux -> next;
+    }
+
+}
+
+void    ft_index_stack(t_list **stack)
+{
+    t_list  *aux;
+    int     index;
+
+    index = 0;
+    aux = ft_min_value(stack);
+    while (aux)
+    {
+        aux -> index = index++;
+        aux = ft_min_value(stack);
+    }
+}
