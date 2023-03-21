f2ec44b16efbbb48b4ab62c7a9aef4828780a995
Sara Alvarado Díaz <sr.lilitha@MBP-de-Sara.home>
2023-03-21 16:00:46 +0100

    Add ft_swap.c

---
+++ ft_swap.c
@@ -0,0 +1,43 @@
+#include "push_swap.h"
+
+int ft_swap(t_list **stack)
+{
+    int     aux_value;
+    int     aux_index;
+
+    if (ft_lstsize(*stack) < 2)
+        return (-1);
+    aux_value = *stack -> value;
+    aux_index = *stack -> index;
+    *stack -> value = *stack -> next -> value;
+    *stack -> index = *stack -> next -> index;
+    *stack -> next -> value = aux_value;
+    *stack -> next -> index = aux_index;
+    return (0);
+}
+
+int sa(t_list **stack_a)
+{
+    if(ft_swap(stack_a) == -1)
+        return (-1);
+    ft_putendl_fd("sa", 1);
+    return (0);
+}
+
+int sb(t_list **stack_b)
+{
+    if(ft_swap(stack_b) == -1)
+        return (-1);
+    ft_putendl_fd("sb", 1);
+    return (0);
+}
+
+int ss(t_list **stack_a, t_list **stack_b)
+{
+    if ((ft_lstsize(*stack_a) < 2) || (ft_lstsize(*stack_b) < 2))
+        return (-1);
+    ft_swap(stack_a);
+    ft_swap(stack_b);
+    ft_putendl_fd("ss", 1);
+    return (0);
+}
