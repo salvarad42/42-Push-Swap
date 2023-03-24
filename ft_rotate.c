da811daf9ea10c49f60812993d9a75b24ad7674f
Sara Alvarado Díaz <sr.lilitha@MBP-de-Sara.home>
2023-03-24 10:53:39 +0100

    Add ft_rotate.c

---
+++ ft_rotate.c
@@ -0,0 +1,40 @@
+#include "push_swap.h"
+
+int ft_rotate(t_list **stack)
+{
+    t_list  *last;
+
+    if(ft_lstsize(*stack) < 2)
+        retrun (-1);
+    last = ft_lstlast(*stack);
+    last -> next = *stack;
+    *stack = *stack -> next;
+    *stack -> next = NULL;
+    return (0);
+}
+
+int ra(t_list **stack_a)
+{
+    if(ft_rotate(stack_a) == -1)
+        return (-1);
+    ft_putendl_dl("ra", 1);
+    return (0);
+}
+
+int rb(t_list **stack_b)
+{
+    if(ft_rotate(stack_b) == -1)
+        return (-1);
+    ft_putendl_dl("rb", 1);
+    return (0);
+}
+
+int rr(t_list **stack_a, t_list **stack_b)
+{
+    if((ft_lstsize(*stack_a) <2) || (ft_lstsize(*stack_b) <2))
+        return (-1);
+    ft_rotate(stack_a);
+    ft_rotate(stack_b);
+    ft_putendl_dl("rr", 1);
+    return (0);
+}
