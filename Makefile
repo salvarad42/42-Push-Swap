2d4d37cbf94c4dd47ca6dd06f5a25fbeed7bb5a4
Sara Alvarado Díaz <sr.lilitha@MBP-de-Sara.home>
2023-04-26 14:43:42 +0200

    fixed

--- Makefile
+++ Makefile
@@ -1,23 +1,37 @@
 NAME = push_swap
+
 SRC = ft_check_args.c ft_check_errors.c ft_free.c ft_index_stack.c\
 	ft_is_sorted.c ft_make_stack.c ft_radix_sort.c ft_simple_sort.c\
 	ft_sort_stack.c push_swap.c ft_push.c ft_swap.c ft_rotate.c\
 	ft_reverse.c
+
 OBJS = ${SRC:.c=.o}
-CC = gcc
-CFLAGS = -Wall -Wextra -Werror -stdlib=libstdc++
+
+CFLAGS = -Wall -Wextra -Werror
+
+LIBFT = ./libft
+
+LIBFT.A = ./libft/libft.a
+
 RM = rm -rf
+
+CC = gcc
+
 all: ${NAME}
-${NAME}: ${OBJS}
-		@${MAKE} -C ./libft
-		@${CC} ${CFLAGS} ${OBJS} ./libft/libft.a -o ${NAME}
+
+${NAME}: ${OBJS} ${LIBFT.A}
+		@${CC} ${CFLAGS} ${OBJS} ${LIBFT.A} -o ${NAME}
+
+${LIBFT.A}:
+		@${MAKE} -C ${LIBFT}
+
 clean:
-		@${MAKE} -C ./libft fclean
+		@${MAKE} -C ${LIBFT} fclean
 		@${RM} ${OBJS}
+
 fclean: clean
 		@${RM} ${NAME}
-re: fclean all
-.PHONY: all clean fclean re
 
-debug: CFLAGS += -g
-debug: ${NAME}
+re: fclean all
+
+.PHONY: all clean fclean re
--- ft_simple_sort.c
+++ ft_simple_sort.c
@@ -1,88 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_simple_sort.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: salvarad <salvarad@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/24 10:54:32 by salvarad          #+#    #+#             */
+/*   Updated: 2023/04/24 11:02:44 by salvarad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void ft_sort_3(t_list **stack)
+int	ft_pos_min(t_list *stack)
 {
-    if ((*stack) -> index == 0)
-    {
-        ra(stack);
-        sa(*stack);
-        rra(stack);
-    }
-    else if ((*stack) -> index == 1)
-    {
-        if ((*stack) -> next -> index == 0)
-            sa(*stack);
-        else
-            rra(stack);
-    }
-    else if ((*stack) -> index == 2)
-    {
-        if ((*stack) -> next -> index == 0)
-            ra(stack);
-        else
-        {
-            sa(*stack);
-            rra(stack);
-        }
-    }
+	int	pos;
+	int	min;
+	int	i;
+
+	i = 0;
+	pos = i;
+	min = stack -> index;
+	while (stack)
+	{
+		if (stack -> index < min)
+		{
+			min = stack -> index;
+			pos = i;
+		}
+		i++;
+		stack = stack -> next;
+	}
+	return (pos);
 }
 
-void    ft_sort_4(t_list **stack_a, t_list *stack_b)
+void	ft_sort_3(t_list **stack, int pos_min)
 {
-    if ((*stack_a) -> next -> index == 0)
-        sa(*stack_a);
-    else if ((*stack_a) -> next -> next -> index == 0)
-    {
-        ra(stack_a);
-        ra(stack_a);
-    }
-    else if (ft_lstlast(*stack_a) -> index == 0)
-        rra(stack_a);
-    if (ft_is_sorted(*stack_a))
-        return ;
-    pb(stack_a, &stack_b);
-    ft_sort_3(stack_a);
-    pa(stack_a, &stack_b);
+	if (pos_min == 0)
+	{
+		ra(stack);
+		sa(*stack);
+		rra(stack);
+	}
+	else if (pos_min == 1)
+	{
+		if ((*stack) -> index < (*stack) -> next -> next -> index)
+			sa(*stack);
+		else
+			ra(stack);
+	}
+	else if (pos_min == 2)
+	{
+		if ((*stack) -> index < (*stack) -> next -> index)
+			rra(stack);
+		else
+		{
+			sa(*stack);
+			rra(stack);
+		}
+	}
 }
 
-void    ft_sort_5(t_list **stack_a, t_list *stack_b)
+void	ft_sort_4(t_list **stack_a, t_list *stack_b, int pos_min)
 {
-    if ((*stack_a) -> next -> index == 0)
-        sa(*stack_a);
-    else if ((*stack_a) -> next -> next -> index == 0)
-    {
-        ra(stack_a);
-        ra(stack_a);
-    }
-    else if ((*stack_a) -> next -> next -> next -> index == 0)
-    {
-        rra(stack_a);
-        rra(stack_a);
-    }
-    else if (ft_lstlast(*stack_a) -> index == 0)
-        rra(stack_a);
-    if (ft_is_sorted(*stack_a))
-        return ;
-    pb(stack_a, &stack_b);
-    ft_sort_4(stack_a, stack_b);
-    pa(stack_a, &stack_b);
+	if (pos_min == 1)
+		sa(*stack_a);
+	else if (pos_min == 2)
+	{
+		ra(stack_a);
+		ra(stack_a);
+	}
+	else if (pos_min == 3)
+		rra(stack_a);
+	if (ft_is_sorted(*stack_a))
+		return ;
+	pb(stack_a, &stack_b);
+	ft_sort_3(stack_a, ft_pos_min(*stack_a));
+	pa(stack_a, &stack_b);
 }
 
-void    ft_simple_sort(t_list **stack_a)
+void	ft_sort_5(t_list **stack_a, t_list *stack_b, int pos_min)
 {
-    t_list  *stack_b;
-    int     size;
+	if (pos_min == 1)
+		sa(*stack_a);
+	else if (pos_min == 2)
+	{
+		ra(stack_a);
+		ra(stack_a);
+	}
+	else if (pos_min == 3)
+	{
+		rra(stack_a);
+		rra(stack_a);
+	}
+	else if (pos_min == 4)
+		rra(stack_a);
+	if (ft_is_sorted(*stack_a))
+		return ;
+	pb(stack_a, &stack_b);
+	ft_sort_4(stack_a, stack_b, ft_pos_min(*stack_a));
+	pa(stack_a, &stack_b);
+}
 
-    size = ft_lstsize(*stack_a);
-    stack_b = NULL;
-    if (ft_is_sorted(*stack_a) || size < 2)
-        return;
-    if (size == 2)
-        sa(*stack_a);
-    if (size == 3)
-        ft_sort_3(stack_a);
-    if (size == 4)
-        ft_sort_4(stack_a, stack_b);
-    if (size == 5)
-        ft_sort_5(stack_a, stack_b);
+void	ft_simple_sort(t_list **stack_a)
+{
+	t_list	*stack_b;
+	int		size;
+	int		pos_min;
+
+	size = ft_lstsize(*stack_a);
+	pos_min = ft_pos_min(*stack_a);
+	stack_b = NULL;
+	if (ft_is_sorted(*stack_a) || size < 2)
+		return ;
+	if (size == 2)
+		sa(*stack_a);
+	if (size == 3)
+		ft_sort_3(stack_a, pos_min);
+	if (size == 4)
+		ft_sort_4(stack_a, stack_b, pos_min);
+	if (size == 5)
+		ft_sort_5(stack_a, stack_b, pos_min);
 }
